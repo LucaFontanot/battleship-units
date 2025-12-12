cb6a3fedd34b037d966e74cac8cfb95a9a480d07
Rickypanta0 <rickypanta.dev@gmail.com>
2025-12-12 21:47:54 +0100

    Enhance tests for Grid class
    Signed-off-by: Rickypanta0 <rickypanta.dev@gmail.com>

--- src/test/java/TestGrid.java
+++ src/test/java/TestGrid.java
@@ -16,6 +16,18 @@ public class TestGrid {
         assertThrowsExactly(IllegalArgumentException.class, () -> new Grid(0, 5));
     }
 
+    @Test
+    public void testGridCreation_RectangulareDimensions(){
+        Grid grid = new Grid(2, 3);
+        CellStates emptyCell = CellStates.EMPTY;
+        CellStates[][] expected = {
+                {emptyCell, emptyCell, emptyCell},
+                {emptyCell, emptyCell, emptyCell}
+        };
+        assertTrue(Arrays.deepEquals(expected, grid.getGrid()), "The grid is not initialized correctly.");
+
+    }
+
     @BeforeEach
     void setUp() {
         grid = new Grid(5, 5);
@@ -38,6 +50,11 @@ public class TestGrid {
     public void testChangeGridCellState(){
         grid.changeState(new Coordinate(2,2), CellStates.HIT);
         assertEquals(CellStates.HIT, grid.getGrid()[2][2], "The grid cell state is not updated correctly.");
+    }
+
+    @Test
+    public void testChangeGridCellState_OutOfBounds(){
+        assertThrowsExactly(IndexOutOfBoundsException.class, () -> grid.changeState(new Coordinate(-1,-1), CellStates.HIT));
     }
 
     @Test
@@ -64,5 +81,31 @@ public class TestGrid {
         String expected = "00000000000000X0M0000000X";
 
         assertEquals(expected, grid.gridSerialization(), "The grid is not serialized correctly.");
+    }
+
+    @Test
+    public void testEmptyGridSerialization() {
+        String expectedSerialization = "0000000000000000000000000";
+        assertEquals(expectedSerialization, grid.gridSerialization(), "Empty grid serialization failed.");
+    }
+
+    @Test
+    public void testFullGridSerialization() {
+        Grid grid = new Grid(2, 2);
+        grid.changeState(new Coordinate(0, 0), CellStates.HIT);
+        grid.changeState(new Coordinate(0, 1), CellStates.MISS);
+        grid.changeState(new Coordinate(1, 0), CellStates.MISS);
+        grid.changeState(new Coordinate(1, 1), CellStates.HIT);
+        String expectedSerialization = "XMMX";
+        assertEquals(expectedSerialization, grid.gridSerialization(), "Full grid serialization failed.");
+    }
+
+    @Test
+    public void testGridSerializationNoOverwrites() {
+        grid.changeState(new Coordinate(0, 0), CellStates.HIT);
+        grid.changeState(new Coordinate(0, 1), CellStates.HIT);
+        grid.changeState(new Coordinate(0, 0), CellStates.EMPTY);
+        String expectedSerialization = "0X00000000000000000000000";
+        assertEquals(expectedSerialization, grid.gridSerialization(), "Serialization with a reverted cell state failed.");
     }
 }
