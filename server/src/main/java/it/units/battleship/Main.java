1a15985df513b3d8019507983bda8092225d1523
luca <admin@lucaservers.com>
2025-12-13 19:24:43 +0100

    feat: Added entrypoint for the server

--- server/src/main/java/it/units/battleship/Main.java
+++ server/src/main/java/it/units/battleship/Main.java
@@ -1,7 +1,23 @@
 package it.units.battleship;
 
+import it.units.battleship.service.CommandLineService;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            CommandLineService cls = new CommandLineService(args);
+            if (cls.isHelpRequested()){
+                //
+            } else if (cls.isVersionRequested()){
+               //
+            } else{
+                WebServerApp webServerApp = new WebServerApp(cls.getServerPort());
+                Thread serverThread = new Thread(webServerApp);
+                serverThread.start();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
