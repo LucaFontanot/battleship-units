3ae1ad600c4addcf45bf422a9a8b005658c4a71e
luca <admin@lucaservers.com>
2025-12-13 19:32:07 +0100

    feat: Added help command

--- server/src/main/java/it/units/battleship/Main.java
+++ server/src/main/java/it/units/battleship/Main.java
@@ -3,20 +3,22 @@ package it.units.battleship;
 import it.units.battleship.service.CommandLineService;
 import org.apache.commons.cli.ParseException;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         try {
             CommandLineService cls = new CommandLineService(args);
             if (cls.isHelpRequested()){
-                //
+                cls.printHelp();
             } else if (cls.isVersionRequested()){
-               //
+               System.out.println("Battleship Server Version: " + BuildConstants.VERSION);
             } else{
                 WebServerApp webServerApp = new WebServerApp(cls.getServerPort());
                 Thread serverThread = new Thread(webServerApp);
                 serverThread.start();
             }
-        } catch (ParseException e) {
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
     }
