07f5cb75343658368b4177d739eb56fc1a394c33
Luca Fontanot <contact@lucafontanot.it>
2025-12-15 17:13:03 +0100

    fix: SetProperty env for log4j should be done in the main

--- server/src/main/java/it/units/battleship/Main.java
+++ server/src/main/java/it/units/battleship/Main.java
@@ -1,12 +1,14 @@
 package it.units.battleship;
 
 import it.units.battleship.service.CommandLineService;
+import it.units.battleship.service.PathManager;
 import org.apache.commons.cli.ParseException;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("programPath", PathManager.getProgramPath().resolve("logs").toAbsolutePath().normalize().toString());
         try {
             CommandLineService cls = new CommandLineService(args);
             if (cls.isHelpRequested()){
