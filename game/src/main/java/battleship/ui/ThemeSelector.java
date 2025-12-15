6ba878c6c6c27149b3729bd8bfedad4a100fa195
Luca Fontanot <contact@lucafontanot.it>
2025-12-15 16:22:16 +0100

    feat: Added theme selector

---
+++ game/src/main/java/battleship/ui/ThemeSelector.java
@@ -0,0 +1,37 @@
+package battleship.ui;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
+
+/**
+ * Utility class for selecting and applying UI themes.
+ */
+public class ThemeSelector {
+
+    /**
+     * Selects and applies the theme based on the operating system's current theme setting.
+     */
+    public static void selectAutomaticTheme() {
+        OsThemeDetector detector = OsThemeDetector.getDetector();
+        if (detector.isDark()){
+            selectDarkTheme();
+        } else {
+            selectLightTheme();
+        }
+    }
+
+    /**
+     * Applies the dark theme.
+     */
+    public static void selectDarkTheme() {
+        FlatDarkLaf.setup();
+    }
+
+    /**
+     * Applies the light theme.
+     */
+    public static void selectLightTheme() {
+        FlatLightLaf.setup();
+    }
+}
