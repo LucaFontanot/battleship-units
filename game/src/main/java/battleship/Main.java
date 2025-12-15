d963c1b3d2bdbda0633a85e327cfb9f7df8cdbb2
Luca Fontanot <contact@lucafontanot.it>
2025-12-15 17:16:13 +0100

    Added WelcomeUI

--- game/src/main/java/battleship/Main.java
+++ game/src/main/java/battleship/Main.java
@@ -1,7 +1,32 @@
 package battleship;
 
+import battleship.ui.ThemeSelector;
+import battleship.ui.welcome.WelcomeUi;
+import it.units.battleship.BuildConstants;
+import it.units.battleship.Logger;
+import it.units.battleship.service.CommandLineService;
+import it.units.battleship.service.PathManager;
+import org.apache.commons.cli.ParseException;
+
+import java.io.IOException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.setProperty("programPath", PathManager.getProgramPath().resolve("logs").toAbsolutePath().normalize().toString());
+        try {
+            CommandLineService cls = new CommandLineService(args);
+            if (cls.isHelpRequested()) {
+                cls.printHelp();
+            } else if (cls.isVersionRequested()) {
+                System.out.println("Battleship Server Version: " + BuildConstants.VERSION);
+            } else {
+                Logger.setDebugEnabled(true);
+                ThemeSelector.selectAutomaticTheme();
+                new WelcomeUi().show();
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
