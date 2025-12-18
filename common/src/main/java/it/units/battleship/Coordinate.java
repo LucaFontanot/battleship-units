e884ab5fe7f05cf4debfaa8489a19f346ff3c2c8
Rickypanta0 <rickypanta.dev@gmail.com>
2025-12-18 17:23:33 +0100

    Bring Coordinate record in common directory
    Signed-off-by: Rickypanta0 <rickypanta.dev@gmail.com>

--- common/src/main/java/it/units/battleship/Coordinate.java
+++ common/src/main/java/it/units/battleship/Coordinate.java
@@ -1,3 +1,3 @@
-package battleship.model;
+package it.units.battleship;
 
 public record Coordinate(int row, int col) {}
--- common/src/main/java/it/units/battleship/data/socket/WebSocketMessage.java
+++ common/src/main/java/it/units/battleship/data/socket/WebSocketMessage.java
@@ -2,6 +2,11 @@ package it.units.battleship.data.socket;
 
 import lombok.*;
 
+/**
+ * A generic class representing a WebSocket message.
+ *
+ * @param <T> the type of data that the message contains
+ */
 @Getter
 @Setter
 @AllArgsConstructor
