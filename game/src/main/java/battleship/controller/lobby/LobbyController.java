12fb02b09c7b87ac9f722a3f94e68c806e6825ce
luca <admin@lucaservers.com>
2026-02-07 13:58:38 +0100

    feat: Added LobbyController

---
+++ game/src/main/java/battleship/controller/lobby/LobbyController.java
@@ -0,0 +1,64 @@
+package battleship.controller.lobby;
+
+import battleship.controller.http.JsonHttpController;
+import battleship.controller.http.JsonHttpException;
+import it.units.battleship.Defaults;
+import it.units.battleship.Logger;
+import it.units.battleship.data.LobbiesResponseData;
+import it.units.battleship.data.LobbyData;
+import it.units.battleship.data.PingResponseData;
+
+public class LobbyController {
+    final JsonHttpController<PingResponseData, Void> pingResponse = new JsonHttpController<>(PingResponseData.class, Void.class);
+    final JsonHttpController<LobbiesResponseData, Void> lobbiesController = new JsonHttpController<>(LobbiesResponseData.class, Void.class);
+    final JsonHttpController<LobbyData, Void> lobbyCreateController = new JsonHttpController<>(LobbyData.class, Void.class);
+
+    /**
+     * Pings the server to check if it's alive and retrieves the ping response data.
+     * @return the PingResponseData if the ping is successful, or null if an error occurs
+     */
+    public PingResponseData ping() {
+        try {
+            return pingResponse.getSync(Defaults.HTTP_PING_ENDPOINT);
+        } catch (JsonHttpException e) {
+            Logger.error("Failed to ping server: " + e.getMessage());
+            Logger.exception(e);
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves the list of lobbies from the server.
+     * @return the LobbiesResponseData containing the list of lobbies, or null if an error occurs
+     */
+    public LobbiesResponseData getLobbies() {
+        try {
+            return lobbiesController.getSync(Defaults.HTTP_LOBBY_ENDPOINT);
+        } catch (JsonHttpException e) {
+            Logger.error("Failed to get lobbies: " + e.getMessage());
+            Logger.exception(e);
+            return null;
+        }
+    }
+
+    /**
+     * Creates a new lobby on the server with the given name and player.
+     * @param name the name of the lobby to be created
+     * @param player the name of the player creating the lobby
+     * @return the LobbyData of the newly created lobby, or null if an error occurs
+     */
+    public LobbyData createLobby(String name, String player) {
+        try {
+            LobbyData requestData = LobbyData.builder()
+                    .lobbyName(name)
+                    .playerOne(player)
+                    .build();
+            return lobbyCreateController.postSync(Defaults.HTTP_LOBBY_ENDPOINT, requestData);
+        } catch (JsonHttpException e) {
+            Logger.error("Failed to create lobby: " + e.getMessage());
+            Logger.exception(e);
+            return null;
+        }
+    }
+
+}
