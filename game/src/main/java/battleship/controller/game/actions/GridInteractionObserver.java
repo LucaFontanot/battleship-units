292ee395b6482116a15a7bc974663ec9afc33007
Rickypanta0 <rickypanta.dev@gmail.com>
2026-02-10 21:53:28 +0100

    Add docs for signature methods
    Signed-off-by: Rickypanta0 <rickypanta.dev@gmail.com>

--- game/src/main/java/battleship/controller/game/actions/GridInteractionObserver.java
+++ game/src/main/java/battleship/controller/game/actions/GridInteractionObserver.java
@@ -2,8 +2,19 @@ package battleship.controller.game.actions;
 
 import it.units.battleship.Coordinate;
 
+/**
+ * Observer interface for monitoring user interactions with the game grid.
+ * Used to notify determined components when a cell is hovered or click
+ */
 public interface GridInteractionObserver {
 
+    /**
+     * Called when the mouse hovers over a grid cell.
+     */
     void onGridHover(Coordinate coordinate);
+
+    /**
+     * Called when a grid cell is clicked.
+     */
     void onGridClick(Coordinate coordinate);
 }
