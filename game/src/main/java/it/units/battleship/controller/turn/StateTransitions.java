c459c5428b5e4398be6b8b49f54f97b5371a6879
Rickypanta0 <rickypanta.dev@gmail.com>
2026-02-11 22:52:51 +0100

    refactor: extract state-transition-related methods into StateTransitions interface
    Signed-off-by: Rickypanta0 <rickypanta.dev@gmail.com>

--- game/src/main/java/it/units/battleship/controller/turn/StateTransitions.java
+++ game/src/main/java/it/units/battleship/controller/turn/StateTransitions.java
@@ -1,4 +1,8 @@
 package it.units.battleship.controller.turn;
 
-public interface StateTransition {
+public interface StateTransitions {
+    void transitionToActiveTurn();
+    void transitionToWaitingOpponent();
+    void transitionToWaitingSetup();
+    void transitionToGameOver(boolean won, String message);
 }
